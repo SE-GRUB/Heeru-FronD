0de6473a0c0e9b5cc700dae7c8a67f72d1b452bc
theAxels <angkianto.laurence@binus.ac.id>
2024-02-02 10:07:57 +0700

    validasi password dan validasi no_telp

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let nip = "";
 let no_telp = "";
 let user_id = "";
 let username = "";
+let email = "";
 
 var histhost
 
@@ -38,6 +39,10 @@ function initpoin() {
                 localStorage.setItem('no_telp', no_telp);
                 user_id = alldata.user['id'];
                 localStorage.setItem('user_id', user_id);
+                email = alldata.user['email'];
+                localStorage.setItem('email', email);
+                password = alldata.user['password'];
+                localStorage.setItem('password', password);
                 if (inputField.value == nip) {
                     // errortext.classList.remove("hide");
                     window.location.href = "./sign_up2.html";
@@ -59,11 +64,16 @@ function initpoin2(){
         var inputField = document.getElementById("PhoneNumberinput");
         var errortext = document.getElementById("errortext")
         
-        if (inputField.value.trim() === "") {
+        var phoneNumber = inputField.value.trim();
+        if (phoneNumber === "") {
             errortext.innerHTML="Please input your Phone Number"
             errortext.classList.remove("hide")
             return false;
         }else{
+            if (phoneNumber.charAt(0) === '8') {
+                inputField.value = '0' + phoneNumber;
+            }
+
             if (inputField.value==phonenumber) {
                 window.location.href="./otp_signup.html"            
             }else{
@@ -81,6 +91,14 @@ function initpoin3() {
 
     dataNama.innerHTML = names;
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var emailInput = document.getElementById("emailinput");
+        var storedEmail = localStorage.getItem('email');
+        if (storedEmail !== null) {
+            emailInput.value = storedEmail;
+        }
+    });
+
     document.getElementById("submitBtn").addEventListener("click", async function() {
         var email = document.getElementById("emailinput");
         var password = document.getElementById('passwordinput');
@@ -89,7 +107,8 @@ function initpoin3() {
 
         var errortext=document.getElementById("errortext");
         var errortext2=document.getElementById("errortext2");
-        var errortext3=document.getElementById("errortext3");
+        var errortext3=document.getElementById("errortext3");        
+        
         
         if (email.value.trim() === "") {
             errortext.innerHTML = "Please input your email address";
@@ -103,32 +122,50 @@ function initpoin3() {
                 // return false;
             }else{
                 errortext.classList.add('hide');
+                if (password.value.trim() === "") {
+                    errortext2.innerHTML="Please input your Password"
+                    errortext2.classList.remove("hide")
+                }else{
+                    if(!/[A-Z]/.test(password.value)){
+                        errortext2.innerText = "Your password must contain at least one uppercase letter";
+                        errortext2.classList.remove("hide");
+                    }{
+                        if (password.value.length < 8 ) {
+                            errortext2.innerHTML = "Password must be at least 8 characters long";
+                            errortext2.classList.remove("hide");
+                        } else {
+                            if(!/[a-z]/.test(password.value)){
+                                errortext2.innerText = "Your password must contain at least one lowercase letter";
+                                errortext2.classList.remove("hide");
+                            }else{
+                                if(!/\d/.test(password.value)){
+                                    errortext2.innerText = "Your password must contain at least one number.";
+                                    errortext2.classList.remove("hide");
+                                }else{
+                                    errortext2.classList.add('hide');
+                                    if (password_confirmation.value.trim() === "") {
+                                        errortext3.innerHTML = "Please input your password confirmation";
+                                        errortext3.classList.remove("hide");
+                                        // return false;
+                                    } else {
+                                        
+                                        if (password.value !== password_confirmation.value) {
+                                                errortext3.innerHTML = "Your password and password confirmation do not match";
+                                                errortext3.classList.remove("hide");
+                                            } else {
+                                                await requestdata(`updateProfile?user_id=${localStorage.getItem('user_id')}&email=${email.value}&password=${password.value}`)
+                                                window.location.href = "./MainApk/home.html";
+                                                return true;
+                                            }
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-
-        if (password.value.trim() === "") {
-            errortext2.innerHTML="Please input your Password"
-            errortext2.classList.remove("hide")
-            // return false;
-        }else{
-            errortext2.classList.add('hide');
-        }
-
-        if (password_confirmation.value.trim() === "") {
-            errortext3.innerHTML = "Please input your password confirmation";
-            errortext3.classList.remove("hide");
-            // return false;
-        } else {
-            if (password.value !== password_confirmation.value) {
-                errortext3.innerHTML = "Your password and password confirmation do not match";
-                errortext3.classList.remove("hide");
-                // return false;
-            }else{
-                await requestdata(`updateProfile?user_id=${localStorage.getItem('user_id')}&email=${email.value}&password=${password.value}`)
-                window.location.href = "./MainApk/home.html";
-            }
-        }
-    
         return false;
     });
 }
