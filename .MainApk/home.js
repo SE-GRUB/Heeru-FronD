bb88e8da88b0b77135e859afe4043c8651c2e226
theAxels <127481633+theAxels@users.noreply.github.com>
2024-02-06 13:55:58 +0000

    angki poin true

---
+++ .MainApk/home.js
@@ -0,0 +1,13 @@
+var dataNama = document.getElementById("databaseName");
+var dataLike = document.getElementById("databaseJumlahLike")
+var datareply = document.getElementById("databaseJumlahReply")
+
+var names = 'Felicia_hua';
+var sumlike = 100;
+var sumreply = 10;
+
+
+dataNama.innerHTML = names;
+dataLike.innerHTML = sumlike;
+datareply.innerHTML = sumreply;
+
