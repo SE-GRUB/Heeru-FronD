7dfc30a59061456c1159c5c82246b356bbbd3ba3
Yohanna Y <yohyoe18@gmail.com>
2024-02-20 11:19:54 +0700

    sudahbisaLOOPING_POSTNYA

--- MainApk/home.js
+++ MainApk/home.js
@@ -11,3 +11,12 @@ dataNama.innerHTML = names;
 dataLike.innerHTML = sumlike;
 datareply.innerHTML = sumreply;
 
+
+// Looping Komen
+var container = document.getElementById("badanpost");
+var box = document.getElementById("kotakpost");
+
+for (var i = 0; i < 3; i++) {
+    container.appendChild(box.cloneNode(true));
+}
+
